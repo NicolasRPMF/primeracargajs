d38632c37b76e10a0090ee07630e41c09948957e
Nicolas Rodriguez <fg9221393@gmail.com>
2023-12-12 21:14:27 -0300

    Primera carga

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+let invitacion = prompt("¿Quieres unos tragos?");
+
+if (invitacion.toLowerCase() === "si") {
+    alert("¡Genial, paso enseguida!");
+} else if (invitacion.toLowerCase() === "no") {
+    alert("Bueno, está bien").break;
+} else {
+    alert("ERROR, RESPONDE 'SI' O 'NO', vuelve a cargar").break;
+}
+
+let num1 = prompt("Si me has acompañado a beber, ¿cuántas consumiste?");
+let num2 = prompt("Mi cantidad fue de...");
+
+function calcularTotal() {
+    return parseInt(num1) + parseInt(num2);
+}
+
+alert("Bebimos " + calcularTotal() + " tragos");
+
+const escabio = ["Fernet", "Pinta", "Daikiri", "DrLemon"];
+
+alert("Estos fueron: " + escabio.join(", "));
+
+const olvido = prompt("¿Olvidaste algún trago? Si es así, ¿cuál fue?");
+
+escabio.push(olvido);
+alert("Al final bebimos: " + escabio.join(", "));
+
+
+
+
